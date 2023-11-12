bd1e0f69c162af0930921e2ec803075fcdc007f0
Tanmay Seth <kushal.seth2010@gmail.com>
2023-11-12 12:03:07 +0530

    Adding new comment

--- app/src/main/java/com/example/androidappqrscanner/MainActivity.java
+++ app/src/main/java/com/example/androidappqrscanner/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 showCamera();
                }else{
                    //show why we need permission
+                   //We can do anything here
                }
             });
     private ActivityResultLauncher<ScanOptions> qrCodeLauncher = registerForActivityResult(new ScanContract(),result ->{
